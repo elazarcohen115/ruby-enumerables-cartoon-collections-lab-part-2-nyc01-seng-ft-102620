ec5eb41b67f8d0453a0417dee9136c6ecaf54265
simon cohen <escohen115@gmail.com>
2020-10-19 00:15:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,11 +12,13 @@ def summon_captain_planet(planeteer_calls)
     new << word.capitalize
   end
   new
-  # Use an Enumerable to capitalize and add '!' to every element in the passed in array
-  # Return a new array of the results
 end
 
 def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? do |word|
+    word.length > 4
+  end
+
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
 end
